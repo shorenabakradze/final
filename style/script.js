805e933b482f330ee3b375d22708c96898002eba
shorenabakradze <136688121+shorenabakradze@users.noreply.github.com>
2023-07-25 22:03:00 +0400

    add

---
+++ style/script.js
@@ -0,0 +1,10 @@
+
+let navbar = document.getElementById('navbar');
+let navbar = document.getElementById('burgerbar');
+
+burger.addEventListener('click', function() {
+    navbar.classList.add('activenav-bar')
+}
+
+
+)
